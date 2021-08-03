6f7deddde92f77c87d87ecc3c7fb90d05e313527
ArtyomHomePc <1274646a1@gmail.com>
2021-08-03 23:44:20 +0300

    layout logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import s from "./Header.module.css"
+import s from './Header.module.css'
 
 const Header = () => {
     return (
         <header className={s.header}>
-            <img src="/img/logo.png" alt=""/>
-            <p>Social Network</p>
+            <p><img src="/img/logo.png" alt=""/> Social Network</p>
             <div className={s.header}>Login</div>
         </header>
     );
