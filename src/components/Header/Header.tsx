ad946b2949ab48daa5e663c42bba972ff33d841e
ArtyomHomePc <1274646a1@gmail.com>
2021-08-04 00:33:17 +0300

    layout logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import s from './Header.module.css'
 const Header = () => {
     return (
         <header className={s.header}>
-            <p><img src="/img/logo.png" alt=""/> Social Network</p>
+            <p><img src={'/img/logo.png'} alt=""/>Social Network</p>
             <div className={s.header}>Login</div>
         </header>
     );
