accd60b30351381d46d9c7cca8f4e00ba1422d1d
ArtyomHomePc <1274646a1@gmail.com>
2021-08-05 16:23:47 +0300

    customize textarea, button 'Add Post'

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -5,12 +5,9 @@ import Post from './Post/Post';
 const MyPosts = () => {
     return (
         <div className={s.post}>
-            <div>
-                New Post
-            </div>
-            <div>
-                <textarea/>
-                <button>Add Post</button>
+            <div className={s.addPost}>
+                <textarea className={s.textArea} placeholder='Your New Post'/>
+                <button className={s.button}>Add Post</button>
             </div>
             <Post message = 'Hi' likeCount={0}/>
             <Post message= 'Bye' likeCount={10}/>
