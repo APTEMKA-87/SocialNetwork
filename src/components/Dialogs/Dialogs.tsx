73a9fbfbe40a6d063a48f3cda39af80a84f7ff14
ArtyomHomePc <1274646a1@gmail.com>
2021-08-22 16:52:40 +0300

    dialogData, postData lesson 24

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -5,7 +5,7 @@ import {NavLink} from 'react-router-dom';
 
 type DialogItemType = {
     name: string
-    id: string,
+    id: number,
 }
 
 let DialogItem = (props: DialogItemType) => {
@@ -19,6 +19,7 @@ let DialogItem = (props: DialogItemType) => {
 
 type MessageType = {
     message: string
+    id: number
 }
 
 let Message = (props: MessageType) => {
@@ -26,19 +27,31 @@ let Message = (props: MessageType) => {
 }
 
 const Dialogs = () => {
+
+    let dialogsData = [
+        {id: 1, name: 'Dima'},
+        {id: 2, name: 'Sveta'},
+        {id: 3, name: 'Victor'},
+        {id: 4, name: 'Valera'},
+        {id: 5, name: 'Ignat'},
+        {id: 6, name: 'Jenya'}
+    ]
+
+    let messageData = [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'How r u?'},
+        {id: 3, message: 'Thank`s'}
+    ]
+
     return (
         <div className={s.content}>
             <div className={s.dialogItems}>
-                <DialogItem name="Dima" id="1"/>
-                <DialogItem name="Sveta" id="2"/>
-                <DialogItem name="Victor" id="3"/>
-                <DialogItem name="Valera" id="4"/>
-                <DialogItem name="Ignat" id="5"/>
+                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
             </div>
             <div className={s.messages}>
-                <Message message="Hi"/>
-                <Message message="How r u?"/>
-                <Message message="Thank`s"/>
+                <Message message={messageData[0].message} id={messageData[0].id}/>
+                <Message message={messageData[1].message} id={messageData[1].id}/>
             </div>
         </div>
     );
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -3,14 +3,20 @@ import s from './MyPosts.module.css'
 import Post from './Post/Post';
 
 const MyPosts = () => {
+
+    let postData = [
+        {id: 1, post: 'Hi', likesCount: 0},
+        {id: 2, post: 'Bye', likesCount: 10}
+    ]
+
     return (
         <div className={s.post}>
             <div className={s.addPost}>
                 <textarea className={s.textArea} placeholder='Your New Post'/>
                 <button className={s.button}>Add Post</button>
             </div>
-            <Post message = 'Hi' likeCount={0}/>
-            <Post message= 'Bye' likeCount={10}/>
+            <Post message={postData[0].post} likeCount={postData[0].likesCount}/>
+            <Post message={postData[1].post} likeCount={postData[1].likesCount}/>
         </div>
     );
 };
