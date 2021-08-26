6338ad2d3d52c175a51ba7f01f9afa4a3afb3311
ArtyomHomePc <1274646a1@gmail.com>
2021-08-26 22:02:27 +0300

    addButtonMessage, lesson 31

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,6 +3,7 @@ import s from './Dialogs.module.css'
 import DialogItem from './DialogItem/DialogItem';
 import MessageDialogs from './Message/Message';
 import { DialogPageType } from '../../Redux/State';
+import n from './../Profile/MyPosts/MyPosts.module.css'
 
 type DialogsPropsType ={
     dialogPage: DialogPageType
@@ -12,7 +13,12 @@ const Dialogs = (props: DialogsPropsType) => {
 
     let dialogElements = props.dialogPage.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>)
     let messagesElements = props.dialogPage.messages.map(m => <MessageDialogs messageDialog={m.message} />)
+    let sendMessage = React.createRef<HTMLTextAreaElement>()
 
+    let addButtonMessage = () => {
+        let text = sendMessage.current?.value
+        alert(text)
+    }
     return (
         <div className={s.content}>
             <div className={s.dialogItems}>
@@ -20,6 +26,8 @@ const Dialogs = (props: DialogsPropsType) => {
             </div>
             <div className={s.messages}>
                 {messagesElements}
+                <textarea className={n.textArea} placeholder="New Message" ref={sendMessage}/>
+                <button className={n.button} onClick={addButtonMessage}>Send Message</button>
             </div>
         </div>
     );
