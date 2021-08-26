762637b2c89b9f12f65d455524d2955b1942ebac
ArtyomHomePc <1274646a1@gmail.com>
2021-08-26 21:05:42 +0300

    onclick, addButtonPost, ref, lesson 31

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -16,11 +16,18 @@ const MyPosts: React.FC<MyPostsPropsType> = (props) => {
 
     let postsElement = props.posts.map(p => <Post postFromPost={p.post} likeCount={p.likesCount}/>)
 
+    let newPostElement = React.createRef<HTMLTextAreaElement>()
+
+    let addButtonPost = () => {
+        let text = newPostElement.current?.value
+        alert(text)
+    }
+
     return (
         <div className={s.post}>
             <div className={s.addPost}>
-                <textarea className={s.textArea} placeholder='Your New Post'/>
-                <button className={s.button}>Add Post</button>
+                <textarea className={s.textArea} placeholder="Your New Post" ref={newPostElement} />
+                <button className={s.button} onClick={addButtonPost}>Add Post</button>
             </div>
             {postsElement}
         </div>
