29d5f3646cfd4edae478c494b75a91da4c4491a1
ArtyomHomePc <1274646a1@gmail.com>
2021-10-01 21:04:49 +0300

    lesson 44

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,15 @@
 import './index.css';
-import store from './Redux/redux-store';
 import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './App';
+import store from './Redux/redux-store';
 import {Provider} from './StoreContext';
 
-export const rerenderTree = () => {
-    ReactDOM.render(
-        <React.StrictMode>
-            <Provider store={store}>
-                <App/>
-            </Provider>
-        </React.StrictMode>,
-        document.getElementById('root')
-    );
-}
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
+);
 
-rerenderTree()
-store.subscribe(() => {
-    rerenderTree()
-})
 
