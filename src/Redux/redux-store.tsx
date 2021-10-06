af42f5feed186bfff78b0da887dd5b6c70c72674
ArtyomHomePc <1274646a1@gmail.com>
2021-10-06 20:54:16 +0300

    start lesson 46

--- src/Redux/redux-store.tsx
+++ src/Redux/redux-store.tsx
@@ -2,12 +2,12 @@ import {combineReducers, createStore, Store} from 'redux';
 import ProfileReducer from './profile-reducer';
 import DialogsReducer from './dialogs-reducer';
 
-let reducers = combineReducers({
+let rootReducer = combineReducers({
     profilePage:ProfileReducer,
     dialogPage:DialogsReducer
 })
-export type RootStateType = ReturnType<typeof reducers>
-let store: Store = createStore(reducers)
+export type RootStateType = ReturnType<typeof rootReducer>
+let store: Store = createStore(rootReducer)
 
 export default store;
 
