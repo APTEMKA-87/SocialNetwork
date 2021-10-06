4a361d5068e01441f72d3ca701139efb91b8f8d4
ArtyomHomePc <1274646a1@gmail.com>
2021-10-06 23:00:04 +0300

    Users.tsx start lesson 48

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,19 @@ import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import {BrowserRouter, Route} from 'react-router-dom';
 import DialogContainer from './components/Dialogs/DialogContainer';
+import Users from './components/Users/Users';
 
 function App() {
     return <BrowserRouter>
         <div className="app-wrapper">
             <Header/>
             <Navbar/>
-            <Route path="/dialogs" render={() => <DialogContainer/>}/>
-            <Route path="/profile" render={() => <Profile/>}/>
+            <Route path="/dialogs"
+                   render={() => <DialogContainer/>}/>
+            <Route path="/profile"
+                   render={() => <Profile/>}/>
+            <Route path="/users"
+                   render={() => <Users/>}/>
         </div>
     </BrowserRouter>;
 }
---
+++ src/components/Users/Users.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Users = () => {
+    return (
+        <div>
+            fuck
+        </div>
+    );
+};
+
+export default Users;
