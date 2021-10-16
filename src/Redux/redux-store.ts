61cf55cb5e89f305e490aefcc65a14646ae14a69
ArtyomHomePc <1274646a1@gmail.com>
2021-10-16 20:41:13 +0300

    finished lesson 54

--- src/Redux/redux-store.ts
+++ src/Redux/redux-store.ts
@@ -2,6 +2,7 @@ import {combineReducers, createStore, Store} from 'redux';
 import ProfileReducer from './profile-reducer';
 import DialogsReducer from './dialogs-reducer';
 import usersReducer from './user-reducer';
+import {composeWithDevTools} from 'redux-devtools-extension';
 
 let rootReducer = combineReducers({
     profilePage:ProfileReducer,
@@ -9,7 +10,7 @@ let rootReducer = combineReducers({
     usersPage: usersReducer
 })
 export type RootStateType = ReturnType<typeof rootReducer>
-let store: Store = createStore(rootReducer)
+let store: Store = createStore(rootReducer, composeWithDevTools())
 
 export default store;
 
