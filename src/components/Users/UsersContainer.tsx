83a4943783d233594a45e283fb84faee6d265864
ArtyomHomePc <1274646a1@gmail.com>
2021-10-17 21:06:19 +0300

    lesson 57 start

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -1,4 +1,3 @@
-import UsersApiComponent from './UsersApiComponent';
 import {connect} from 'react-redux';
 import {RootStateType} from '../../Redux/redux-store';
 import {
@@ -10,6 +9,9 @@ import {
     UserType
 } from '../../Redux/user-reducer';
 import {Dispatch} from 'redux';
+import React from 'react';
+import axios from 'axios';
+import Users from './Users';
 
 type MapStateToPropsType = {
     users: Array<UserType>
@@ -24,6 +26,56 @@ type MapDispatchToProps = {
     setUsers: (users: Array<UserType>) => void
     setCurrentPage: (pageNumber: number) => void
     setTotalUsersCount: (totalCount: number) => void
+}
+
+type PropsType = {
+    users: Array<UserType>
+    totalUserCount: number
+    setUsers: (users: Array<UserType>) => void
+    pageSize: number
+    currenPage: number
+    setCurrentPage: (pageNumber: number) => void
+    setTotalUsersCount: (totalCount: number) => void
+    follow: (userId: number) => void
+    unfollow: (userId: number) => void
+
+}
+
+type ResponseType = {
+    items: Array<UserType>,
+    totalCount: number,
+    error: string
+}
+
+class UsersContainer extends React.Component <PropsType, any> {        // type?
+
+    componentDidMount() {
+        axios.get<ResponseType>(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currenPage}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items)
+            this.props.setTotalUsersCount(response.data.totalCount)
+        })
+    }
+
+    onPostChanged = (pageNumber: number) => {
+        this.props.setCurrentPage(pageNumber)
+        axios.get<ResponseType>(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items)
+        })
+    }
+
+    render() {
+
+
+        return <Users totalUserCount={this.props.totalUserCount}
+                      pageSize={this.props.pageSize}
+                      currenPage={this.props.currenPage}
+                      onPostChanged={this.onPostChanged}
+                      users={this.props.users}
+                      follow={this.props.follow}
+                      unfollow={this.props.unfollow}
+
+        />
+    }
 }
 
 let mapStateToProps = (state: RootStateType): MapStateToPropsType => {
@@ -55,4 +107,4 @@ let mapDispatchToProps = (dispatch: Dispatch): MapDispatchToProps => {
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(UsersApiComponent); //?
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer); //?
