326c8383aef3414f569ed19be6a6c7886c273c32
ArtyomHomePc <1274646a1@gmail.com>
2021-10-17 17:36:40 +0300

    lesson 55 add setCurrentPageAC

--- src/Redux/user-reducer.ts
+++ src/Redux/user-reducer.ts
@@ -28,6 +28,7 @@ const initialState = {
 const FOLLOW = 'FOLLOW'
 const UNFOLLOW = 'UNFOLLOW'
 const SET_USERS = 'SET_USERS'
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE'
 
 const usersReducer = (state: UsersType = initialState, action: ActionsUsersType): UsersType => {
     switch (action.type) {
@@ -54,19 +55,27 @@ const usersReducer = (state: UsersType = initialState, action: ActionsUsersType)
         case SET_USERS: {
             return {...state, users: [...state.users, ...action.users]}
         }
+        case SET_CURRENT_PAGE: {
+            return {...state, currenPage: action.currenPage}
+        }
         default:
             return state
     }
 }
 
-export type ActionsUsersType = followACType | unfollowACType | setUsersACType
+export type ActionsUsersType = followACType
+    | unfollowACType
+    | setUsersACType
+    | setCurrentPageACType
 
 export type followACType = ReturnType<typeof followAC>
 export type unfollowACType = ReturnType<typeof unfollowAC>
 export type setUsersACType = ReturnType<typeof setUsersAC>
+export type setCurrentPageACType = ReturnType<typeof setCurrentPageAC>
 
 export const followAC = (userId: number) => ({type: FOLLOW, userId} as const)
 export const unfollowAC = (userId: number) => ({type: UNFOLLOW, userId} as const)
 export const setUsersAC = (users: Array<UserType>) => ({type: SET_USERS, users} as const)
+export const setCurrentPageAC = (currenPage: number) => ({type: SET_CURRENT_PAGE, currenPage} as const)
 
 export default usersReducer
