c23866966c1a8fb4dc3478da54e93066d75ca09a
ArtyomHomePc <1274646a1@gmail.com>
2021-10-23 20:15:17 +0300

    lesson 61 setUserData

--- src/Redux/auth-reducer.ts
+++ src/Redux/auth-reducer.ts
@@ -13,18 +13,19 @@ let initialState = {
 const SET_USER_DATA = 'SET_USER_DATA'
 
 const authReducer = (state: initialStateType = initialState, action: ActionAuthType) => {
-switch (action.type) {
-    case 'SET_USER_DATA':
-        return {
-            ...state
-        }
-}
+    switch (action.type) {
+        case 'SET_USER_DATA':
+            return {
+                ...state,
+                ...action.data
+            }
+    }
 }
 
 export type ActionAuthType = setUserDataACType
 
 export type setUserDataACType = ReturnType<typeof setUserData>
 
-export const setUserData = () => ({type: SET_USER_DATA} as const)
+export const setUserData = (id: number, email: string, login: string) => ({type: SET_USER_DATA,data: { id, email, login}} as const)
 
 export default authReducer;
