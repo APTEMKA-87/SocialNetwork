190c2e9600861e510fdb830fec184f91a5b0b67b
ArtyomHomePc <1274646a1@gmail.com>
2021-12-25 13:06:33 +0300

    lesson 75 add redux-form

--- src/Redux/redux-store.ts
+++ src/Redux/redux-store.ts
@@ -2,18 +2,19 @@ import {applyMiddleware, combineReducers, createStore, Store} from 'redux';
 import ProfileReducer from './profile-reducer';
 import DialogsReducer from './dialogs-reducer';
 import usersReducer from './user-reducer';
-import {composeWithDevTools} from 'redux-devtools-extension';
 import authReducer from './auth-reducer';
 import thunkMiddleware from 'redux-thunk';
+import {reducer as formReducer} from 'redux-form';
 
 let rootReducer = combineReducers({
     profilePage:ProfileReducer,
     dialogPage:DialogsReducer,
     usersPage: usersReducer,
     auth: authReducer,
+    form: formReducer,
 })
 export type RootStateType = ReturnType<typeof rootReducer>
-let store: Store = createStore(rootReducer/*, composeWithDevTools()*/, applyMiddleware(thunkMiddleware))
+let store: Store = createStore(rootReducer, applyMiddleware(thunkMiddleware))
 
 export default store;
 
